9367e95d42495c1b9c9011fad250ead1189e9258
Oubowen <aobshchyn@gmail.com>
2020-10-03 22:13:24 +0300

    feat: implement reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  return (Math.sign(n) === -1) ?
+    n.toString().split('').splice(1).reverse().join('') :
+    n.toString().split('').reverse().join('') * Math.sign(n);
 }
